44227aa778684112e4f9a2933e2755ff3e84b58a
Paulo César Jr <paulomurdock@gmail.com>
2020-07-07 01:31:46 -0300

    finaliando Curriculo 0.4

--- script.js
+++ script.js
@@ -1,23 +1,83 @@
-var botaoAdicionar = document.querySelector("#exp");
+var botaoAdicionar = document.querySelector("#adicionar");
+var listElement = document.querySelector("#experiencias");
 
-botaoAdicionar.addEventListener("click",()=>{
-    event.prevenliefault();
+var listaExperiencia = JSON.parse(localStorage.getItem("lista_experiencia")) || [{
+    cargo: "alguma coisa",
+    empresa: "outra coisa",
+    descricao: "mais outra coisa"
+}];
 
-    const nome = ["Item1", "Item2", "Item3"];
+botaoAdicionar.addEventListener("click", () => {
+    event.preventDefault();
 
-    const dados = document.querySelector("#dadosPesssoas");
+    const inputCargo = document.querySelector("#cargo")
+    const inputEmpresa = document.querySelector("#empresa")
+    const inputDescricao = document.querySelector("#descricao")
 
-    const ul = document.createElement("ul");
+    listaExperiencia.push({
+        cargo: inputCargo.value,
+        empresa: inputEmpresa.value,
+        descricao: inputDescricao.value
+    });
 
-    const li1= document.createElement("li");
-    const li2= document.createElement("li");
+    console.log(listaExperiencia);
+    inputCargo.value = ""
+    inputEmpresa.value = ""
+    inputDescricao.value = ""
 
-    li1.innerText = "Meu Primeiro Item";
-    li2.innerText = "Meu Segundo Item";
-
-    ul.append(li1);
-    ul.append(li2);
-
-    dados.append(ul);
-
+    saveToStorage();
+    renderExp();
 });
+
+function renderExp() {
+    listElement.innerHTML = "";
+
+    for (exp of listaExperiencia) {
+        var expElement = document.createElement("li");
+        expElement.className= "experiencia"
+        var expCargoP = document.createElement("p")
+        expCargoP.className = "titulo5"
+        var expEmpresaP = document.createElement("p")
+        expEmpresaP.className = "titulo2"
+        var expDescricaoP = document.createElement("p")
+        expDescricaoP.className = "titulo4"
+        var expCargo = document.createTextNode(exp.cargo);
+        var expEmpresa = document.createTextNode(exp.empresa);
+        var expDescricao = document.createTextNode(exp.descricao);
+
+        expCargoP.appendChild(expCargo);
+        expEmpresaP.appendChild(expEmpresa);
+        expDescricaoP.appendChild(expDescricao);
+
+        expElement.appendChild(document.createElement("span"))
+        expElement.appendChild(expCargoP);
+        expElement.appendChild(expEmpresaP);
+        expElement.appendChild(expDescricaoP);
+        expElement.appendChild(elementDeleteButton(exp));
+
+        listElement.appendChild(expElement);
+    }
+}
+
+function elementDeleteButton(exp) {
+    var pos = listaExperiencia.indexOf(exp);
+
+    var linkElement = document.createElement("img");
+    linkElement.setAttribute("src", "https://image.flaticon.com/icons/svg/833/833520.svg#");
+    linkElement.setAttribute("onclick", "deleteExp(" + pos + ")");
+    linkElement.className="deleteButton";
+    
+    return linkElement;
+}
+
+function deleteExp(pos) {
+    listaExperiencia.splice(pos, 1);
+    renderExp();
+    saveToStorage();
+}
+
+function saveToStorage() {
+    localStorage.setItem("lista_experiencia",JSON.stringify(listaExperiencia));
+}
+
+renderExp();
